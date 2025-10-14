9dea627a7be826b1e7ebd3e10e400716f78b7c16
Harineko0 <harineko0927@gmail.com>
2025-10-15 01:14:54 +0900

    Migrate to Biome

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,17 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    define: {
-        'import.meta.vitest': false,
-    },
-    plugins: [
-        tsconfigPaths(),
-    ],
-    test: {
-        globals: true,
-        include: ['test/**/*.test.ts'],
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'json', 'json-summary', 'html'],
-        },
-    },
+	define: {
+		"import.meta.vitest": false,
+	},
+	plugins: [tsconfigPaths()],
+	test: {
+		globals: true,
+		include: ["test/**/*.test.ts"],
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "json", "json-summary", "html"],
+		},
+	},
 });

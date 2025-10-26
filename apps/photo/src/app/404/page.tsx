8f4f3357bebe994888433a4d1709ba319f1d8d90
Nagomu0128 <kyoshidk@gmail.com>
2025-10-27 00:37:43 +0900

    空ページにコンテンツを追加

---
+++ apps/photo/src/app/404/page.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center p-24">
+			<h1 className="text-5xl font-bold mb-8">404 - Page Not Found</h1>
+		</main>
+	);
+}
--- apps/photo/tailwind.config.ts
+++ apps/photo/tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./src/components/**/*.{ts,tsx}",
     "./src/libs/i18n/**/*.{ts,tsx}",
   ],
-  darkMode: ["class"],
+  darkMode: "class",
   theme: {
     extend: {},
   },

a01be82736891c46ad9718c06dbd35c869ed81a6
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-11-07 16:01:16 +0900

    Google Sans のファイル追加と import

---
+++ apps/photo/src/lib/font.ts
@@ -0,0 +1,36 @@
+import localFont from "next/font/local";
+
+export const googleSans = localFont({
+	src: [
+		{
+			path: "../../public/fonts/GoogleSans-Regular.woff2",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/GoogleSans-Italic.woff2",
+			weight: "400",
+			style: "italic",
+		},
+		{
+			path: "../../public/fonts/GoogleSans-Medium.woff2",
+			weight: "500",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/GoogleSans-MediumItalic.woff2",
+			weight: "500",
+			style: "italic",
+		},
+		{
+			path: "../../public/fonts/GoogleSans-Bold.woff2",
+			weight: "700",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/GoogleSans-BoldItalic.woff2",
+			weight: "700",
+			style: "italic",
+		},
+	],
+});
